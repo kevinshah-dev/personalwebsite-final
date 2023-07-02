748a8f0ae7b14dd49f8cf532a002c425164aa4ec
Kevin Shah <ks539@duke.edu>
2023-07-02 15:48:16 -0400

    Added a contact section

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -20,4 +20,46 @@ export default function Contact() {
     const email = () => {
         window.open("ks539@duke.edu", "_blank", "noreferrer,noopener");
     }
+
+    return (
+        <>
+        <Container maxW={"3xl"} id="contact">
+        <Stack
+          as={Box}
+          textAlign={"center"}
+          spacing={{ base: 8, md: 14 }}
+          pb={{ base: 20, md: 36 }}
+        >
+          <Stack align="center" direction="row" p={4}>
+            <HStack mx={4}>
+              <Text color={`${color}.400`} fontWeight={800}>
+                04
+              </Text>
+              <Text fontWeight={800}>Contact</Text>
+            </HStack>
+            <Divider orientation="horizontal" />
+          </Stack>
+          <Stack spacing={4} as={Container} maxW={"3xl"} textAlign={"center"}>
+            <Heading fontSize={"3xl"}>Let's stay in touch!</Heading>
+            <Text color={`${color}.500`} fontWeight={600} fontSize={"lg"} px={4}>
+              ks539@duke.edu
+            </Text>
+            <Center>
+              <HStack pt={4} spacing={4}>
+                <FaLinkedin onClick={linkedin} size={28} />
+                <FaGithub onClick={github} size={28} />
+                <FaEnvelope onClick={email} size={28} />
+              </HStack>
+            </Center>
+          </Stack>
+        </Stack>
+      </Container>
+        
+        
+        </>
+    )
 }
+
+
+
+
